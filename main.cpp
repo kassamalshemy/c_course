00dafb8e4360d9245df455d69c9115886ca887a8
kassam <kassamalshemy@gmail.com>
2025-09-10 23:30:28 +0300

    changing the main file modify...

--- main.cpp
+++ main.cpp
@@ -2,12 +2,16 @@
 
 using namespace std; 
 
-
+void modify()
+{
+    cout << "modified the main file to see the git thing"; 
+}
 
 int main()
 {
     cout << "hello world!\n"; 
-    
+    modify();
+
 
     return 0; 
 }
